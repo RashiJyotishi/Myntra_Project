e996c4d5a216654bdb5d16b11ae6ab756225367a
Nandini Arora <nandini.arora.che22@itbhu.ac.in>
2024-07-15 11:39:08 +0530

    word cloud

--- client/src/Components/WordCloud.jsx
+++ client/src/Components/WordCloud.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 import WordCld from "../assets/WordCloud/wordcloud.png"
 const WordCloud = () => {
   return (
-    <div className='h-[40vh] flex flex-col items-center justify-center rounded-2xl drop-shadow-sm'>
-        <img src={WordCld} alt="" />
+    <div className=' flex flex-col justify-center w-[100vw] h-[88vh] bg-pink-50 px-36'>
+      <h1 className='text-5xl font-semibold text-pink-600 mb-10'>Word Cloud</h1>
+      <h1 className='text-xl font-regular text-orange-600 mb-10'> <b className='mb-10'>Need: What is GenZ looking for?</b> <br /> <span className='font-regular'>Solution: Get the information of latest and trending hashtags, cloth items, and a summary of words to attract GenZ on Social Media. Knowing what GenZ is searching for matters. Be at tight place and right time to be discovered.</span> </h1>
+        <img src={WordCld} alt="" className='rounded-3xl drop-shadow-sm'/>
     </div>
   )
 }
--- client/src/HomePage.jsx
+++ client/src/HomePage.jsx
@@ -3,12 +3,12 @@ import { Link } from 'react-router-dom';
 import Hero from "../src/assets/Hero.png"
 const HomePage = () => {
   return (
-    <div className="h-[90vh] w-[100vw] py-36 px-24 bg-pink-50">
+    <div className="h-[80vh] w-[100vw] py-32 px-24 bg-pink-50">
       <div className='flex flex-row items-center justify-between'>
         <div>
           <img src={Hero} alt="" />
         </div>
-        <div className='pl-10'>
+        <div className='pl-16'>
           <h1 className='text-7xl text-pink-600 font-semibold'>Welcome to the Myntra Trend Dashboard </h1>
           <ul className='text-orange-600 text-lg mt-10'>
             <li>1. Get Latest Instagram, Pinterest and other Social Media Fashion Trends, to know what Genz is wearing.</li>
